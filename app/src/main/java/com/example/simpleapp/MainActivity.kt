7898f3d39fd38255ae414f830c0acb07cf5805de
YunByungKwan <pk7955@naver.com>
2020-05-07 23:50:55 +0900

    feat: Add Permission and dialog method

--- app/src/main/java/com/example/simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/simpleapp/MainActivity.kt
@@ -1,81 +1,53 @@
 package com.example.simpleapp
 
-import android.Manifest
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
+import android.provider.MediaStore
 import com.example.simpleapp.util.Utils
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var receiver: BroadcastReceiver
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val utils = Utils()
+        val utils = Utils(this, this) /** 수정 필요 */
 
-        toastButton1.setOnClickListener {
-            utils.showShortToast(this, "short toast")
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // utils.showDialog("권한 필요", "앱을 사용하기 위해 해당 권한 승인이 필요합니다.", "확인", "취소")
+        // utils.checkPermission(Manifest.permission.CAMERA)
+
+        btn_network.setOnClickListener {
+            utils.showNetworkState()
         }
 
-        toastButton2.setOnClickListener {
-            utils.showLongToast(this, "long toast")
-        }
-
-        snackbarButton1.setOnClickListener {
-            utils.showShortSnackbar(it, "short snackbar")
-        }
-
-        snackbarButton2.setOnClickListener {
-            utils.showLongSnackbar(it, "long snackbar")
-        }
-
-        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val view = inflater.inflate(R.layout.dialog, null)
-
-        val alertDialog = AlertDialog.Builder(this)
-            .setTitle("제목")
-            .setPositiveButton("저장") { dialog, which ->
-                Toast.makeText(applicationContext, "긍정", Toast.LENGTH_SHORT).show()
-            }
-            .setNegativeButton("취소", null)
-            .create()
-
-        alertDialog.setView(view)
-        alertDialog.show()
-        /*
-        val dialog = AlertDialog.Builder(this)
-        dialog.setTitle("제목")
-        dialog.setMessage("친구목록에서 지우시겠습니까?")
-        val dialog_listener = DialogInterface.OnClickListener { dialog, which ->
-            when(which) {
-                DialogInterface.BUTTON_POSITIVE ->
-                    Toast.makeText(applicationContext, "긍정", Toast.LENGTH_SHORT).show()
-                DialogInterface.BUTTON_NEGATIVE ->
-                    Toast.makeText(applicationContext, "부정", Toast.LENGTH_SHORT).show()
+        btn_camera.setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                takePictureIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
             }
         }
 
-        dialog.setPositiveButton("YES",dialog_listener)
-        dialog.setNegativeButton("NO",dialog_listener)
-        dialog.show()
-        */
+        btn_gallery.setOnClickListener {
 
-        /**
-         * SharedPreferences
-         */
+        }
+    }
+
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1000
     }
 }
--- app/src/main/java/com/example/simpleapp/util/Utils.kt
+++ app/src/main/java/com/example/simpleapp/util/Utils.kt
@@ -1,102 +1,146 @@
 package com.example.simpleapp.util
 
-import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
-import android.content.Intent
+import android.content.DialogInterface
 import android.content.pm.PackageManager
-import android.net.ConnectivityManager
-import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
-import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 
-class Utils {
+class Utils(_context: Context, _activity: Activity) {
 
-    companion object {
-        private val PERMISSION_ID = 1000
-        private val REQUEST_IMAGE_CAPTURE = 2000
+    private val context = _context
+    private val activity = _activity
+
+    /** Toast */
+
+    /** 짧은 토스트 메시지 출력 */
+    fun showShortToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 
-    private val mNetworkCallback: ConnectivityManager.NetworkCallback = TODO()
-
-    /**
-     * Toast
-     */
-    fun showShortToast(context: Context, msg: String) {
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    /** 긴 토스트 메시지 출력 */
+    fun showLongToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 
-    fun showLongToast(context: Context, msg: String) {
-        Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
-    }
+    /** Snackbar */
 
-    /**
-     * Snackbar
-     */
+    /** 짧은 스낵바 메시지 출력 */
     fun showShortSnackbar(view: View, msg: String) {
         Snackbar.make(view, msg, Snackbar.LENGTH_SHORT).show()
     }
 
+    /** 긴 스낵바 메시지 출력 */
     fun showLongSnackbar(view: View, msg: String) {
         Snackbar.make(view, msg, Snackbar.LENGTH_LONG).show()
     }
 
-    /**
-     * Dialog
-     */
-    fun createAlertDialog(context: Context) {
+    /** Dialog */
+
+    fun showDialog(title: String, message: String) {
         val alertDialog = AlertDialog.Builder(context)
-            .setTitle("제목")
-            .setPositiveButton("저장") { dialog, which ->
-                Toast.makeText(context, "긍정", Toast.LENGTH_SHORT).show()
-            }
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("확인", null)
             .setNegativeButton("취소", null)
             .create()
             .show()
     }
 
-    /**
-     * SharedPreferences
-     */
+    fun showDialog(title: String, message: String, pText: String, nText: String) {
+        val alertDialog = AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(pText, null)
+            .setNegativeButton(nText, null)
+            .create()
+            .show()
+    }
 
-    /**
-     * Permission
-     */
-    fun havePermissionFor(context: Context, permissionName: String): Boolean
-            = (
+    fun showDialog(title: String, message: String, pText: String, nText: String,
+                   pListener: DialogInterface.OnClickListener,
+                   nListener: DialogInterface.OnClickListener) {
+        val alertDialog = AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(pText, pListener)
+            .setNegativeButton(nText, nListener)
+            .create()
+            .show()
+    }
+
+    /** permission */
+
+    /** 해당 권한이 있는지 판별 */
+    fun hasPermissionFor(permissionName: String): Boolean = (
             ContextCompat.checkSelfPermission(context, permissionName)
-            == PackageManager.PERMISSION_GRANTED
-            )
+                    == PackageManager.PERMISSION_GRANTED)
 
-    /**
-     * Network
-     */
-    private fun isNetworkConnected() {
+    /** 해당 권한에 대한 요청을 거부한 적이 있는지 판별 */
+    fun deniedPermission(permissionName: String): Boolean = (
+            ActivityCompat.shouldShowRequestPermissionRationale(activity, permissionName))
 
+    /** 해당 권한 리스트를 요청 */
+    fun requestPermissions(permissionArray: Array<out String>) {
+        ActivityCompat.requestPermissions(activity, permissionArray, REQUEST_PERMISSIONS_CODE)
     }
 
-    private fun requestPermissions(activity: Activity) {
-        ActivityCompat.requestPermissions(
-            activity,
-            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
-            PERMISSION_ID
-        )
+    /** 해당 권한 판별 후 없을 경우 요청 */
+    fun checkPermission(permissionName: String) {
+        if(hasPermissionFor(permissionName)) {
+            Log.d(TAG_PERMISSION, "Permission granted")
+        } else {
+            Log.d(TAG_PERMISSION, "Permission denied")
+
+            if(deniedPermission(permissionName)) { // 거절한적있음
+                Log.d(TAG_PERMISSION, "shouldShowRequestPermissionRationale true")
+
+                /** 권한 필요하다는 다이얼로그 띄우기 */
+                val alertDialog = AlertDialog.Builder(context)
+                    .setTitle("권한 필요")
+                    .setMessage("앱을 사용하기 위해 해당 권한 승인이 필요합니다.")
+                    .setPositiveButton("확인") {dialog, which ->
+                        requestPermissions(arrayOf(permissionName))
+                    }
+                    .setNegativeButton("취소", null)
+                    .create()
+                    .show()
+            } else { // 거절한적 없음
+                Log.d(TAG_PERMISSION, "shouldShowRequestPermissionRationale false")
+
+                requestPermissions(arrayOf(permissionName))
+            }
+
+        }
     }
 
-    private fun dispatchTakePictureIntent(pm: PackageManager, activity: Activity) {
+    /** Network */
 
+    /** 네트워크 상태를 출력 */
+    fun showNetworkState() {
+        if(isNetworkConnected()) {
+            showShortToast("인터넷이 연결되어 있지 않습니다.")
+        } else {
+            showShortToast("인터넷이 연결되어 있습니다.")
+        }
     }
 
-    private fun gps() {
-
+    /** 네트워크 상태를 판별 */
+    fun isNetworkConnected(): Boolean {
+        return false
     }
 
-    private fun fileupload() {
+    companion object {
+        const val REQUEST_PERMISSIONS_CODE = 1000
+        const val REQUEST_IMAGE_CAPTURE = 1001
 
+        private const val TAG_PERMISSION = "Permission check"
+        private const val TAG_NETWORK = "Network state check"
     }
 }
